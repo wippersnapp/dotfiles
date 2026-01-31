0c88204d5cd8ee43f9994e3c706cfb11e2c0fb16
wippersnapp <labtinggie@gmail.com>
2026-01-31 11:18:50 -0500

    Add files via upload

---
+++ xrandr/3monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --mode 1920x1080 --pos 4880x1126 --rotate normal --output HDMI-A-0 --mode 2560x1440 --pos 3440x0 --rotate right --output DisplayPort-0 --primary --mode 3440x1440 --pos 0x605 --rotate normal --output DisplayPort-1 --off
